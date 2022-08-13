b2a14ffb7fbce65e1ff015357d8c7448203ba6e3
glancia <gui.lancia@gmail.com>
2022-08-13 11:20:52 -0300

    improvements

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def test(init_address, inverters, debug=False):
         instrument = set_instrument(init_address+inv, debug=debug)
         try:
             print("Inverter {} - relogio {}".format(init_address+inv, instrument.read_registers(3073-OFFSET, number_of_registers=6, functioncode=4)))  # ano, mes, dia, hora, min, seg
-            print("Inverter {} - param curvas {}".format(init_address + inv, instrument.read_registers(3241 - OFFSET, number_of_registers=3, functioncode=4)))  # ano, mes, dia, hora, min, seg
+            print("Inverter {} - param curvas {}".format(init_address + inv, instrument.read_registers(3240 - OFFSET, number_of_registers=3, functioncode=4)))  # ano, mes, dia, hora, min, seg
         except Exception as e:
             print("Inverter {} - No response".format(init_address+inv))
 
